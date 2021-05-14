46d1167ddee6aa20475ac96257c2bfb1b02f8a17
bxphxmxth <fushs@hotmail.fr>
2021-05-14 10:44:41 +0200

    fin

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,72 @@
-let jour = prompt("On est quel jour");
+// Jours
+// let jour = prompt("On est quel jour");
 
-switch(jour) {
-    case "lundi":
-        alert("J'ai cours le lundi");
+// switch(jour) {
+//     case "lundi":
+//         alert("J'ai cours le lundi");
+//         break;
+//   case "mardi":
+//         alert("Toujours cours");
+//         break;
+//     case "mercredi":
+//         alert("Always, on y est presk");
+//         break;
+//     case "jeudi":
+//         alert("waaah c long");
+//         break;
+//     case "vendredi":
+//         alert("Pq t'es excitée? Tu travailles demain :'(");
+//         break;
+//     case "samedi":
+//         alert("go vendre des legumes bio");
+//         break;
+//     case "dimanche":
+//         alert("Wallah, j'ai plus de vie");
+//         break;
+//     default:
+//         alert("entre un vrai jour de la semaine et pas jeudreudi");
+//         break;   
+// }                             
+
+// Quel temps? 
+
+// let temps = prompt("quel temps fait-il?");
+
+// switch(temps) {
+//     case "pluie":
+//         alert("prends ton paraplouiie");
+//         break;
+//     case "soleil":
+//         alert("crème solaire afou");
+//         break;
+//     default:{
+//         alert("kua?");
+//     }        
+// }
+
+
+// Calculs
+
+let nb1 = prompt("choisis un premier nombre");
+let op = prompt("choisis un opérateur entre +,-,*,/");
+let nb2 = prompt("choisis un deuxième nombre");
+
+switch(op){
+    case "+":
+        alert(`${nb1} + ${nb2} = ${nb1+nb2}`);
         break;
-  case "mardi":
-        alert("Toujours cours");
+    case "-":
+        alert(`${nb1} - ${nb2} = ${nb1-nb2}`);
         break;
-    case "mercredi":
-        alert("Always, on y est presk");
+    case "*":
+        alert(`${nb1} * ${nb2} = ${nb1*nb2}`);
         break;
-    case "jeudi":
-        alert("waaah c long");
-        break;
-    case "vendredi":
-        alert("Pq t'es excitée? Tu travailles demain :'(");
-        break;
-    case "samedi":
-        alert("go vendre des legumes bio");
-        break;
-    case "dimanche":
-        alert("Wallah, j'ai plus de vie");
+    case "/":
+        alert(`${nb1} / ${nb2} = ${nb1/nb2}`);
         break;
     default:
-        alert("entre un vrai jour de la semaine et pas jeudreudi");
-        break;   
-}                             
+        alert("choisis un opérateur correct :(");
+        break;    
+    
+        
+}
