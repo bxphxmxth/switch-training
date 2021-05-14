49a783d9f71171fbad116303a780e6a6e34fa902
bxphxmxth <fushs@hotmail.fr>
2021-05-14 10:35:08 +0200

    s

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+let jour = prompt("On est quel jour");
+
+switch(jour) {
+    case "lundi":
+        alert("J'ai cours le lundi");
+        break;
+  case "mardi":
+        alert("Toujours cours");
+        break;
+    case "mercredi":
+        alert("Always, on y est presk");
+        break;
+    case "jeudi":
+        alert("waaah c long");
+        break;
+    case "vendredi":
+        alert("Pq t'es excitée? Tu travailles demain :'(");
+        break;
+    case "samedi":
+        alert("go vendre des legumes bio");
+        break;
+    case "dimanche":
+        alert("Wallah, j'ai plus de vie");
+        break;
+    default:
+        alert("entre un vrai jour de la semaine et pas jeudreudi");
+        break;   
+}                             
